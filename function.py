3a83032636dc5475950761475e820235149ea7c5
ivanhoe <ayvengo85@gmail.com>
2024-06-02 22:22:02 +0300

    Другий commit

---
+++ function.py
@@ -0,0 +1,11 @@
+# def hello(name):
+#     print('Hello', name)
+#
+# hello('Eugene')
+
+def sum_nums(a, b):
+    sum = a + b
+    return sum
+
+r=sum_nums(3, 10)
+print(r)
--- lesson_1.py
+++ lesson_1.py
@@ -1,13 +1,58 @@
-name = 'Eugene'
-print(dir(name))
-print(name.upper())
+# name = 'Eugene'
+# print(dir(name))
+# print(name.upper())
 
-def fun(name):
-    print(name)
-fun(1)
-
-
-
-
+#
+# a = 12
+# b = 20
+# a, b = b, a
+# print('a=', a, 'b=', b)
 
 
+# name = 'Eugene'
+# print('name=  ', id(name))
+#
+# soname = name
+# print('soname=', id(soname))
+
+# строки
+
+# info_msg = """Привіт
+# Євген
+# Удачи!!!"""
+# print(info_msg)
+# Привіт
+# Євген
+# Удачи!!!
+
+# срези
+# r = "Im Winter hell bis sonnig, im Sommer leicht schattiert, auch im Garten möglich"
+# print(r[10:-1])
+
+# number = int(input("Введіть число:"))
+# print(number)
+# print(type(number))
+# n = float(number)
+# print(n)
+# print(type(n))
+
+# степінь числа
+
+# base = 3
+# pover = 5
+# print(pow(base, pover))
+
+
+#комплексні числа
+
+# complex_a=4 + 5j
+# complex_b=7 + 6j
+# sum=complex_a+complex_b
+# print(sum)
+# print(type(sum))
+
+
+# тип bool
+
+# print(12>11)
+# print('nuts'>'nata')
